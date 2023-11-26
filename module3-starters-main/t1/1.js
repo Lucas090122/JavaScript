6385e7631bba3cdf180d33c452cf0e032c969453
Qingyun Wang <lucas.qingyun@gmail.com>
2023-11-27 01:01:22 +0200

    completed

---
+++ module3-starters-main/t1/1.js
@@ -0,0 +1,7 @@
+const ul = document.querySelector('#target');
+const html =
+    `<li>First item</li>
+    <li>Second item</li>
+    <li>Third item</li>`;
+ul.innerHTML = html;
+ul.className = 'my-list';
