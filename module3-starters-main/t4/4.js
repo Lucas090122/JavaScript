fcfb32f38ffed72dc57024e0ca4c37681798ae8d
Qingyun Wang <lucas.qingyun@gmail.com>
2023-11-27 10:00:34 +0200

    completed

---
+++ module3-starters-main/t4/4.js
@@ -0,0 +1,23 @@
+'use strict';
+const students = [
+  {
+    name: 'John',
+    id: '2345768',
+  },
+  {
+    name: 'Paul',
+    id: '2134657',
+  },
+  {
+    name: 'Jones',
+    id: '5423679',
+  },
+];
+
+const target = document.getElementById('target');
+for (let student of students) {
+  let option = document.createElement('option');
+  option.setAttribute('value', `${student['id']}`);
+  option.textContent = student['name'];
+  target.appendChild(option);
+}
